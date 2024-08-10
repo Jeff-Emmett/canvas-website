8a9809f2a35de715d6042dac0c8941da111ba3e3
Jeff Emmett <46964190+Jeff-Emmett@users.noreply.github.com>
2024-08-10 01:43:09 -0400

    board commit

---
+++ src/components/Board.tsx
@@ -0,0 +1,16 @@
+
+import { Editor, Tldraw } from "@tldraw/tldraw";
+import { canvas } from "@/canvas01";
+
+export function Board() {
+  console.log("HELOOOOOOO")
+  return (
+    <div className="tldraw__editor">
+      <Tldraw
+        onMount={(editor: Editor) => {
+          editor.putContentOntoCurrentPage(canvas)
+        }}
+      />
+    </div>
+  );
+}
