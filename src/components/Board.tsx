98dedc0588afae7c2919ae971929b9e8f338b098
Jeff Emmett <46964190+Jeff-Emmett@users.noreply.github.com>
2024-08-10 10:41:35 -0400

    name update

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,7 +3,6 @@ import { Editor, Tldraw } from "@tldraw/tldraw";
 import { canvas } from "@/canvas01";
 
 export function Board() {
-  console.log("HELOOOOOOO")
   return (
     <div className="tldraw__editor">
       <Tldraw
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,14 +3,15 @@ export function Contact() {
     <main>
       <header>
         <a href="/">
-          Orion Reed
+          Jeff Emmett
         </a>
       </header>
       <h1>Contact</h1>
-      <p>Twitter: <a href="https://twitter.com/OrionReedOne">@OrionReedOne</a></p>
-      <p>Mastodon: <a href="https://hci.social/@orion">orion@hci.social</a></p>
-      <p>Email: <a href="mailto:me@orionreed.com">me@orionreed.com</a></p>
-      <p>GitHub: <a href="https://github.com/orionreed">OrionReed</a></p>
+      <p>Twitter: <a href="https://twitter.com/jeffemmett">@jeffemmett</a></p>
+      <p>BlueSky: <a href="https://bsky.app/profile/jeffemmett.bsky.social">@jeffemnmett.bsky.social</a></p>
+      <p>Mastodon: <a href="https://social.coop/@jeffemmett">@jeffemmett@social.coop</a></p>
+      <p>Email: <a href="mailto:jeffemmett@gmail.com">jeffemmett@gmail.com</a></p>
+      <p>GitHub: <a href="https://github.com/Jeff-Emmett">Jeff-Emmett</a></p>
     </main>
 
   );
