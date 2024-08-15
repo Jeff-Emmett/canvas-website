249031619d127c022db78708a825951a89d83018
Jeff Emmett <46964190+Jeff-Emmett@users.noreply.github.com>
2024-08-15 13:48:39 -0400

    commit cal

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,13 +1,27 @@
-
+import { useLocation, useParams } from 'react-router-dom';
 import { Editor, Tldraw } from "@tldraw/tldraw";
 import { canvas } from "@/canvas01";
+import { sharedcalendar2025 } from "@/sharedcalendar2025";
 
 export function Board() {
+  const { pathname } = useLocation();
+  const boardId = pathname.substring(pathname.lastIndexOf('/') + 1);
+  const getBoard = () => {
+    switch (boardId) {
+      case 'canvas01':
+        return canvas;
+      case 'sharedcalendar2025':
+        return sharedcalendar2025;
+      default:
+        return canvas; // Default to canvas if no match
+    }
+  };
+  
   return (
     <div className="tldraw__editor">
       <Tldraw
         onMount={(editor: Editor) => {
-          editor.putContentOntoCurrentPage(canvas as any)
+          editor.putContentOntoCurrentPage(getBoard() as any)
         }}
       />
     </div>
