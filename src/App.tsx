299f3eff87eb0cac8aa005cf9c5e2abb965fd6d6
Jeff Emmett <46964190+Jeff-Emmett@users.noreply.github.com>
2024-12-07 21:28:45 -0500

    remove homepage board

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import { inject } from '@vercel/analytics';
 import "tldraw/tldraw.css";
 import "@/css/style.css"
-import { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { Default } from "@/components/Default";
-import { Canvas } from "@/components/Canvas";
-import { Toggle } from "@/components/Toggle";
-import { useCanvas } from "@/hooks/useCanvas"
-import { createShapes } from "@/utils/utils";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Contact } from "@/components/Contact";
 import { Post } from '@/components/Post';
 import { Board } from './components/Board';
 import { Inbox } from './components/Inbox';
-import { Books } from './components/Books';
 import {
 	Editor,
 	Tldraw,
@@ -32,7 +26,6 @@ inject();
 const customShapeUtils = [ChatBoxShape, VideoChatShape, EmbedShape];
 const customTools = [ChatBoxTool, VideoChatTool, EmbedTool];
 
-// [2]
 export default function InteractiveShapeExample() {
 	return (
 		<div className="tldraw__editor">
@@ -50,7 +43,6 @@ export default function InteractiveShapeExample() {
 	);
 }
 
-// Add this function before or after InteractiveShapeExample
 const handleInitialShapeLoad = (editor: Editor) => {
 	const url = new URL(window.location.href);
 	const shapeId = url.searchParams.get('shapeId') || url.searchParams.get('frameId');
@@ -75,7 +67,6 @@ const handleInitialShapeLoad = (editor: Editor) => {
 				console.log('Zooming to shape bounds');
 				editor.zoomToBounds(editor.getShapeGeometry(shape).bounds, {
 					targetZoom: 1,
-					//padding: 32
 				});
 			}
 		} else {
@@ -94,42 +85,13 @@ function App() {
 		// <React.StrictMode>
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Home />} />
+				<Route path="/" element={<Default />} />
 				<Route path="/card/contact" element={<Contact />} />
 				<Route path="/posts/:slug" element={<Post />} />
 				<Route path="/board/:slug" element={<Board />} />
 				<Route path="/inbox" element={<Inbox />} />
-				<Route path="/books" element={<Books />} />
 			</Routes>
 		</BrowserRouter>
 		// </React.StrictMode>
 	);
 };
-
-function Home() {
-	const { isCanvasEnabled, elementsInfo } = useCanvas();
-	const shapes = createShapes(elementsInfo)
-	const [isEditorMounted, setIsEditorMounted] = useState(false);
-
-	useEffect(() => {
-		const handleEditorDidMount = () => {
-			setIsEditorMounted(true);
-		};
-
-		window.addEventListener('editorDidMountEvent', handleEditorDidMount);
-
-		return () => {
-			window.removeEventListener('editorDidMountEvent', handleEditorDidMount);
-		};
-	}, []);
-
-	return (
-		<>
-			<Toggle />
-			<div style={{ zIndex: 999999 }} className={`${isCanvasEnabled && isEditorMounted ? 'transparent' : ''}`}>
-				{<Default />}
-			</div>
-			{isCanvasEnabled && elementsInfo.length > 0 ? <Canvas shapes={shapes} /> : null}
-		</>
-	)
-}
