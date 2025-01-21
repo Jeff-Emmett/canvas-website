1783d1b6ebc455be3c4f5b9d5ad29c16ba5bdb1b
Jeff-Emmett <jeffemmett@gmail.com>
2025-01-21 23:25:28 +0700

    added scoped propagators (with javascript object on arrow edge to control)

--- src/App.tsx
+++ src/App.tsx
@@ -6,38 +6,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Contact } from "@/routes/Contact"
 import { Board } from "./routes/Board"
 import { Inbox } from "./routes/Inbox"
-import { ChatBoxShape } from "./shapes/ChatBoxShapeUtil"
-import { VideoChatShape } from "./shapes/VideoChatShapeUtil"
-import { ChatBoxTool } from "./tools/ChatBoxTool"
-import { VideoChatTool } from "./tools/VideoChatTool"
-import { EmbedTool } from "./tools/EmbedTool"
-import { EmbedShape } from "./shapes/EmbedShapeUtil"
-import { MycrozineTemplateTool } from './tools/MycrozineTemplateTool'
-import { MycrozineTemplateShape } from './shapes/MycrozineTemplateShapeUtil'
-import { MarkdownShape } from "./shapes/MarkdownShapeUtil"
-import { MarkdownTool } from "./tools/MarkdownTool"
 import { createRoot } from "react-dom/client"
-import { handleInitialPageLoad } from "./utils/handleInitialPageLoad"
 import { DailyProvider } from "@daily-co/daily-react"
 import Daily from "@daily-co/daily-js"
 
-
 inject()
-
-const customShapeUtils = [
-  ChatBoxShape,
-  VideoChatShape,
-  EmbedShape,
-  MycrozineTemplateShape,
-  MarkdownShape,
-]
-const customTools = [
-  ChatBoxTool, 
-  VideoChatTool, 
-  EmbedTool, 
-  // MycrozineTemplateTool, 
-  // MarkdownTool
-]
 
 const callObject = Daily.createCallObject()
 
