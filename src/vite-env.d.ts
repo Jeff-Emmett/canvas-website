12256c5b9ca91f2275627c14742dac9038133bf6
Jeff-Emmett <jeffemmett@gmail.com>
2025-02-13 20:38:01 +0100

    working video calls

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,12 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_TLDRAW_WORKER_URL: string
   readonly VITE_GOOGLE_MAPS_API_KEY: string
-  readonly VITE_DAILY_API_KEY: string
-  readonly VITE_CLOUDFLARE_API_TOKEN: string
-  readonly VITE_CLOUDFLARE_ACCOUNT_ID: string
-  readonly VITE_CLOUDFLARE_ZONE_ID: string
-  readonly VITE_R2_BUCKET_NAME: string
-  readonly VITE_R2_PREVIEW_BUCKET_NAME: string
+  readonly VITE_GOOGLE_CLIENT_ID: string
+  readonly VITE_DAILY_DOMAIN: string
 }
 
 interface ImportMeta {
--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,31 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
 
-export default defineConfig({
-  envPrefix: ["VITE_"],
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-  },
-  build: {
-    sourcemap: true,
-  },
-  base: "/",
-  publicDir: "src/public",
-  resolve: {
-    alias: {
-      "@": "/src",
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    envPrefix: ["VITE_"],
+    plugins: [react()],
+    server: {
+      host: "0.0.0.0",
+      port: 5173,
     },
-  },
-  define: {
-    "import.meta.env.VITE_WORKER_URL": JSON.stringify(
-      process.env.VITE_WORKER_URL,
-    ),
-  },
+    build: {
+      sourcemap: true,
+    },
+    base: "/",
+    publicDir: "src/public",
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
+    },
+    define: {
+      __WORKER_URL__: JSON.stringify(env.VITE_TLDRAW_WORKER_URL),
+      __DAILY_API_KEY__: JSON.stringify(env.VITE_DAILY_API_KEY)
+    }
+  }
 })
