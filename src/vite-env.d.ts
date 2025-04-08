a4c258a1a30ca1b8bd5e3030cf38534ea2217ff2
Jeff-Emmett <jeffemmett@gmail.com>
2025-04-08 14:39:29 -0700

    Update Daily API key in production env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,6 +5,7 @@ interface ImportMetaEnv {
   readonly VITE_GOOGLE_MAPS_API_KEY: string
   readonly VITE_GOOGLE_CLIENT_ID: string
   readonly VITE_DAILY_DOMAIN: string
+  readonly VITE_DAILY_API_KEY: string
 }
 
 interface ImportMeta {
