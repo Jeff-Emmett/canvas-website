db4ae0c766700504a6766acf3591dcba92f6946e
Jeff-Emmett <jeffemmett@gmail.com>
2025-04-08 14:32:15 -0700

    updated website copy, installed locked-view function (coordinates break when locked tho), trying to get video transcripts working

--- worker/worker.ts
+++ worker/worker.ts
@@ -9,13 +9,12 @@ export { TldrawDurableObject } from "./TldrawDurableObject"
 // Define security headers
 const securityHeaders = {
   "Content-Security-Policy":
-    "default-src 'self'; connect-src 'self' wss: https:; img-src 'self' data: blob: https:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline';",
+    "default-src 'self'; connect-src 'self' wss: https:; img-src 'self' data: blob: https:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; frame-src 'self' https://*.daily.co; child-src 'self' https://*.daily.co;",
   "X-Content-Type-Options": "nosniff",
-  "X-Frame-Options": "DENY",
   "X-XSS-Protection": "1; mode=block",
   "Strict-Transport-Security": "max-age=31536000; includeSubDomains",
   "Referrer-Policy": "strict-origin-when-cross-origin",
-  "Permissions-Policy": "camera=(), microphone=(), geolocation=()",
+  "Permissions-Policy": "camera=*, microphone=*, geolocation=()",
 }
 
 // we use itty-router (https://itty.dev/) to handle routing. in this example we turn on CORS because
